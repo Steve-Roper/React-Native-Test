c0e20f8415c36787acfa92790e799fce23bd9f28
Oskar Warnhag <10157509+Steve-Roper@users.noreply.github.com>
2022-03-05 17:00:51 +0100

    Working global state with mobx

--- App.js
+++ App.js
@@ -13,48 +13,106 @@ import {
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
   Button,
   Image,
 } from 'react-native';
-
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {makeAutoObservable} from 'mobx';
+import {observer} from 'mobx-react-lite';
+import {Picker} from '@react-native-picker/picker';
+import {useContext} from 'react/cjs/react.development';
 
 const Stack = createNativeStackNavigator();
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+export class User {
+  id = Math.random();
+  name = '';
+  image = './img/steve.jpg';
+
+  constructor(name) {
+    makeAutoObservable(this);
+    this.name = name;
+  }
+}
+
+export class UserStore {
+  activeUser = null;
+  users = [];
+
+  constructor(users) {
+    makeAutoObservable(this);
+    this.users = users;
+    this.activeUser = users[0];
+  }
+
+  changeActiveUser(user) {
+    this.activeUser = user;
+  }
+}
+
+const StoreContext = React.createContext();
+
+export const StoreProvider: () => Node = ({children, store}) => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
   );
 };
 
-const Home: () => Node = ({navigation}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const UserStoreView = observer(({navigation, userStore}) => {
+  return (
+    userStore &&
+    userStore.users && (
+      <View>
+        <Picker
+          selectedValue={userStore.activeUser}
+          onValueChange={user => userStore.changeActiveUser(user)}>
+          {userStore.users.map(user => (
+            <Picker.Item key={user.id} value={user} label={user.name} />
+          ))}
+        </Picker>
+        {userStore.activeUser && (
+          <View>
+            <Button
+              title="Go to profile page"
+              onPress={() =>
+                navigation.navigate('Profile', {
+                  name: userStore.activeUser.name,
+                })
+              }
+            />
+            <Button
+              title="Go to feed"
+              onPress={() => navigation.navigate('Feed')}
+            />
+          </View>
+        )}
+      </View>
+    )
+  );
+});
+
+const Home: () => Node = ({navigation, isDarkMode = true}) => {
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const store = useContext(StoreContext);
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <UserStoreView navigation={navigation} userStore={store} />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const Profile: () => Node = ({navigation, isDarkMode = true}) => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -64,44 +122,15 @@ const Home: () => Node = ({navigation}) => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Button
-            title="Go to profile page"
-            onPress={() => navigation.navigate('Profile', {name: 'Steve'})}
-          />
-          <Button
-            title="Go to feed"
-            onPress={() => navigation.navigate('Feed')}
-          />
+        <View>
+          <Image source={require('./img/steve.jpg')} />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-const Profile: () => Node = ({navigation, route}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-        contentContainerStyle={styles.profileImageContainer}>
-        <Image source={require('./img/steve.jpg')} />
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const Feed: () => Node = ({navigation}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const Feed: () => Node = ({navigation, isDarkMode = true}) => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -115,17 +144,20 @@ const Feed: () => Node = ({navigation}) => {
   );
 };
 
+const userStore = new UserStore([new User('Steve'), new User('Jane')]);
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home}></Stack.Screen>
-        <Stack.Screen name="Profile" component={Profile}></Stack.Screen>
-        <Stack.Screen name="Feed" component={Feed}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StoreProvider store={userStore}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home}></Stack.Screen>
+          <Stack.Screen name="Profile" component={Profile}></Stack.Screen>
+          <Stack.Screen name="Feed" component={Feed}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StoreProvider>
   );
 };
 
